7cb4754aa6c983087c06c817b8c260362e782244
Stuart Welham <stuart.welham@gmail.com>
2022-02-28 09:34:10 +0000

    Release 0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Apus.MixProject do
   def project do
     [
       app: :apus,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
