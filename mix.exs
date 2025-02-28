ce252b3d3bc712ebf3f05c566a85eae3f18e6a8b
beerlington <pete@lette.us>
2025-02-28 15:18:24 -0500

    Prep for 0.14.1 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Apus.MixProject do
   def project do
     [
       app: :apus,
-      version: "0.14.0",
+      version: "0.14.1",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
